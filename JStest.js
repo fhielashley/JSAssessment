32498a79729fe7193a55d661f90c0f556e0c0715
fhielashley <114235229+fhielashley@users.noreply.github.com>
2023-09-11 11:48:16 +0800

    Update JStest.js

--- JStest.js
+++ JStest.js
@@ -1,12 +1,6 @@
-
-
-// create a variable to hold your NFT's
-
 const NFTs=[]
 
-// this function will take in some values as parameters, create an
-// NFT object using the parameters passed to it for its metadata, 
-// and store it in the variable above.
+
 function mintNFT (_name,_shirt,_pants,_shoes) {
     const NFT= {
         "name": _name,
@@ -19,8 +13,7 @@ function mintNFT (_name,_shirt,_pants,_shoes) {
     console.log("Minted:" + _name);
 }
 
-// create a "loop" that will go through an "array" of NFT's
-// and print their metadata with console.log()
+
 function listNFTs () {
  for(let i=0; i < NFTs.length; i++){
     console.log("\nID:  "+ (i+1))
@@ -31,12 +24,12 @@ function listNFTs () {
  }
 }
 
-// print the total number of NFTs we have minted to the console
+
 function getTotalSupply() {
 console.log(NFTs.length);
 }
 
-// call your functions below this line
+
 mintNFT("Fhiel","CDG","Chrome Heart","Travis Low");
 mintNFT("Nichole","Adidas","Levis","Paris Dunks");
 mintNFT("Ashley","Nike","Guess","Freddy Krugers");
